1b4007fd696f3e9afa70f8d804c126b8adade3c9
Ramyashree Shetty <ramyashree.shetty@blazeclan.com>
2024-04-03 11:49:01 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './views/Home';
+import About from './views/About'; 
+import Contact from './views/Contact'; 
+import NotFound from './views/NotFound'; 
+import Header from './components/Header'; 
+import Footer from './components/Footer';
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          {/* Define routes for different views */}
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          {/* Route for handling 404 errors */}
+          <Route component={NotFound} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
-
+ 
 export default App;
